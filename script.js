df375f622b79d1f770c0b7053e24900b5568ea07
HaroonJewellery <raid.mtc@gmail.com>
2026-02-06 13:32:09 +0400

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+export default {
+  async fetch() {
+    const response = await fetch(
+      "https://www.goldapi.io/api/XAU/USD",
+      {
+        headers: {
+          "x-access-token": GOLDAPI_KEY,
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      }
+    });
+  }
+};
 
